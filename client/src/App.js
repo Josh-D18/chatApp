ffbe9e6fa0705b5638216a468e861e3c248e0ca0
unknown <jjdate@live.com>
2021-09-08 23:07:43 -0400

    finished header and added a protected route function

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import { Component } from "react";
 import "./App.scss";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Friends from "./components/Friends/Friends";
+import Profile from "./components/Profile/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 class App extends Component {
   render() {
@@ -9,7 +13,12 @@ class App extends Component {
       <div className="App">
         <section>
           <Header />
-          <Route></Route>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <ProtectedRoute exact path="/chat" />
+            <ProtectedRoute exact path="/friends" component={Friends} />
+            <ProtectedRoute exact path="/profile" component={Profile} />
+          </Switch>
         </section>
       </div>
     );
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section className="Home">
+      <article>
+        <h1>UChat</h1>
+        <p>Find New Friends to Chat With!</p>
+      </article>
+    </section>
+  );
+}
+
+export default Home;
